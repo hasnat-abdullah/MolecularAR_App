3949728fa24c8f63a018c2e1a939b75da921f149
abdullah <abdullah.2010bd@gmail.com>
2019-03-14 02:04:11 +0600

    solved

---
+++ Assets/Resources/DevPanelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevPanelScript : MonoBehaviour {
+
+	public GameObject DevPanel;
+	int counter;
+	public void showhidePanel(){
+		counter++;
+		if(counter%2==0){
+		DevPanel.gameObject.SetActive(false);
+		}
+		else
+		{
+			DevPanel.gameObject.SetActive(true);
+		}
+	}
+
+
+}
---
+++ Assets/Resources/PanelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelScript : MonoBehaviour {
+
+	public GameObject Panel;
+	int counter;
+	public void showhidePanel(){
+		counter++;
+		if(counter%2==0){
+		Panel.gameObject.SetActive(false);
+		}
+		else
+		{
+			Panel.gameObject.SetActive(true);
+		}
+	}
+
+
+}
